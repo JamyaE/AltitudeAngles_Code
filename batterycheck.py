d3b1b678b32bd96e211035f70b08da11bf2cc318
JamyaE <myacomicsandmore@gmail.com>
2023-04-12 11:16:44 -0400

    very vague outline of what we're going to be doing in the code

---
+++ batterycheck.py
@@ -0,0 +1,9 @@
+import math
+import sys
+import numpy as np
+import time
+
+# have the code be able to read what the battery is while the radio is connected to the computer
+# if the battery is at a certain level, the drone won't fly, or it will
+# have this be connected to the main file to run, and have this connected to the pathfinding file already finished
+# make sure that there's print statement to identify when this code is starting and break up the code
---
+++ mechanismcode.py
@@ -0,0 +1,2 @@
+# this is where the code for the servos will be contained
+# Have the code from MU Editor be connected to our main code
